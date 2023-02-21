c175609a40779047b02303d76d6a7126b9e8ce37
Francisco Garcia <tecatito585@gmail.com>
2023-02-21 12:00:53 -0800

    first commit

---
+++ newAlgoritmos.js
@@ -0,0 +1,13 @@
+function quickCheck(arr, elem) {
+    // Only change code below this line
+    for(let i = 0; i < arr.length; i++){
+        if(arr.indexOf(elem)) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+    // Only change code above this line
+  }
+  
+  console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
